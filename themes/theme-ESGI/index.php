8937a865e2fe334407791989c6dc64b710f7b9ec
Edouard <edouard@voltajazz.com>
2023-05-03 18:38:03 +0200

    Commit 2 séance 1

--- themes/theme-ESGI/index.php
+++ themes/theme-ESGI/index.php
@@ -2,7 +2,16 @@
 
 <main id="site-content">
 	<div class="container">
-		
+		<?php
+		get_template_part('template-parts/identity-card');
+		?>
+
+		<?php 
+		if(!is_front_page()){
+			echo 'pas Accueil !!';
+		}
+		?>
+
 	</div>
 </main>
 
