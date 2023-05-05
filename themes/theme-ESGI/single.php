0edf4cd738bedb451881c2d32d281c7dbbbe20fe
Edouard <edouard@voltajazz.com>
2023-05-05 11:14:58 +0200

    Intégration fin séance 3

--- themes/theme-ESGI/single.php
+++ themes/theme-ESGI/single.php
@@ -2,15 +2,19 @@
 
 <main id="site-content">
 	<div class="container">
-		<h1 class="post-title"><?php the_title() ?></h1>
-		<div class="post-infos">
-			<time><?= get_the_date('j F Y') ?></time>
-			<?= get_avatar($post->post_author) ?>
-			<span><?= get_the_author_meta('display_name', $post->ID) ?></span>
-		</div>
-		<div class="post-image"><?= get_the_post_thumbnail() ?></div>
-		<div class="post-content">
-			<?php the_content() ?>
+		<div class="col-md-6 offset-3">
+			<h1 class="post-title"><?php the_title() ?></h1>
+			<div class="post-infos">
+				<div>
+					<?= get_avatar($post->post_author) ?>
+					<span><?= get_the_author_meta('display_name', $post->post_author) ?></span>
+				</div>
+				<time><?= get_the_date('j F Y') ?></time>
+			</div>
+			<div class="post-image"><?= get_the_post_thumbnail() ?></div>
+			<div class="post-content">
+				<?php the_content() ?>
+			</div>
 		</div>
 	</div>
 </main>
